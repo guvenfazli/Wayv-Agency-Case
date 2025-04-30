30b8a6afc05cc57b352d14348b25c43a07ab2d34
Güven Fazlı <guven.fazli@gmail.com>
2025-05-01 02:40:54 +0300

    feat: login page created

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,9 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Home() {
 
-  const [data, setData] = useState([])
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('http://localhost:8080/campaigns')
-      const resData = await response.json()
-      console.log(resData)
-    }
-
-    try {
-      fetchData()
-
-    } catch (err) {
-      console.log(err)
-    }
-
-  }, [])
 
   return (
     <div>
